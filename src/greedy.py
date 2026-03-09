79fd682e32f87ea910072c66224e1da99de56549
justin <justin8lin@gmail.com>
2026-03-09 11:26:44 -0400

    Set up greedy class

---
+++ src/greedy.py
@@ -0,0 +1,16 @@
+# greedy.py
+
+class Greedy:
+    def __init__(self, k: int, requests: list):
+        self.capacity = k
+        self.sequence = requests
+        
+    
+    def fifo(self): # slide 39
+        pass
+    
+    def lru(self): # slide 39
+        pass
+    
+    def optff(self): # slide 40
+        pass
